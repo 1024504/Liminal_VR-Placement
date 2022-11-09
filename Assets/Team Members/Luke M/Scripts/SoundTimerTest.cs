6a880ac592df5f54c2ab93e7e499ee2687b1e7fc
10213149 <1021349@student.sae.edu.au>
2022-11-10 01:17:19 +1100

    Adding some placeholder sounds

---
+++ Assets/Team Members/Luke M/Scripts/SoundTimerTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTimerTest : MonoBehaviour
+{
+    public float time = 1f; //seconds
+    public AudioSource bubbleSound;
+
+    public void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+        else
+        {
+            Debug.Log("Play Audio Here -- Timer Over!!");
+            this.bubbleSound.Play();
+            time = 1f;
+        }
+    }
+
+}
