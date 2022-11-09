4d57a73cf10019d00818a960ce8b47b69f0986c0
10213149 <1021349@student.sae.edu.au>
2022-11-10 01:44:16 +1100

    SoundUpdate

--- Assets/Team Members/Luke M/Scripts/SoundTimerTest.cs
+++ Assets/Team Members/Luke M/Scripts/SoundTimerTest.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class SoundTimerTest : MonoBehaviour
 {
-    public float time = 1f; //seconds
+    public float time = 7f; //seconds
     public AudioSource bubbleSound;
 
     public void Update()
     {
+        float pitch;
+
         if (time > 0)
         {
             time -= Time.deltaTime;
         }
         else
         {
-            Debug.Log("Play Audio Here -- Timer Over!!");
+            Debug.Log("Bloop!");
+            pitch = Random.Range(0.55f, 1.1f);
+            this.bubbleSound.pitch = pitch;
             this.bubbleSound.Play();
-            time = 1f;
+            time = Random.Range(5f, 16f);      //reset timer
         }
     }
 
