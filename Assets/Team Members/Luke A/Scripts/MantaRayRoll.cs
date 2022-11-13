1aeefc6aab00c867f02c2d67045186e722ae2f0e
Luke Archbold <1024504@student.sae.edu.au>
2022-11-14 10:21:40 +1100

    Luke A Update - Water Shaders, fog particles, moved materials

---
+++ Assets/Team Members/Luke A/Scripts/MantaRayRoll.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MantaRayRoll : SwimmingCreature
+{
+
+	void FixedUpdate()
+	{
+		// _transform.rotation.
+	}
+	
+	
+	
+}
