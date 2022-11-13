6e24edbaea91f1beb8cf9666e295642ad07482aa
Luke Archbold <1024504@student.sae.edu.au>
2022-11-13 21:00:57 +1100

    Luke A Update - Audio Coding

---
+++ Assets/Team Members/Luke A/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Singleton;
+
+	public Action DoThing;
+	
+    // Start is called before the first frame update
+    void Awake()
+    {
+	    Singleton = this;
+    }
+}
--- Assets/Team Members/Luke M/Scripts/SoundTimerTest.cs
+++ Assets/Team Members/Luke M/Scripts/SoundTimerTest.cs
@@ -1,28 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SoundTimerTest : MonoBehaviour
 {
     public float time = 7f; //seconds
     public AudioSource bubbleSound;
 
-    public void Update()
-    {
-        float pitch;
 
-        if (time > 0)
-        {
-            time -= Time.deltaTime;
-        }
-        else
-        {
-            Debug.Log("Bloop!");
-            pitch = Random.Range(0.55f, 1.1f);
-            this.bubbleSound.pitch = pitch;
-            this.bubbleSound.Play();
-            time = Random.Range(5f, 16f);      //reset timer
-        }
+    private void Start()
+    {
+	    CallTimer();
+	    /*GameManager gm = GameManager.Singleton;
+	    gm.DoThing += CallTimer;*/
     }
 
+    void CallTimer()
+    {
+	    StartCoroutine(Timer(time));
+    }
+    
+
+    private IEnumerator Timer(float delay)
+    {
+	    yield return new WaitForSeconds(delay);
+	    Debug.Log("Bloop!");
+	    float pitch = Random.Range(0.55f, 1.1f);
+	    this.bubbleSound.pitch = pitch;
+	    this.bubbleSound.Play();
+	    time = Random.Range(5f, 16f);
+	    CallTimer();
+    }
 }
