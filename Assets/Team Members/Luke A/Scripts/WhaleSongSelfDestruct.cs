d3ae796918b4d513a743a6563840a065751ceab0
Luke Archbold <1024504@student.sae.edu.au>
2022-11-24 11:19:28 +1100

    Luke A Update
    Imported Whale(Placeholder) and Turtle animation
    Created whalesong prefab
    made a mock up animation controller for reference

---
+++ Assets/Team Members/Luke A/Scripts/WhaleSongSelfDestruct.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleSongSelfDestruct : MonoBehaviour
+{
+	private AudioSource source;
+	private float clipLength;
+	
+    void Awake()
+    {
+	    source = GetComponent<AudioSource>();
+	    clipLength = source.clip.length;
+	    Destroy(gameObject, clipLength);
+    }
+}
