336eb4e5176fee2317675b494927f6917663cf6b
Luke Archbold <1024504@student.sae.edu.au>
2022-11-30 17:25:40 +1100

    Luke A Update
    Build for the 30th

---
+++ Assets/Team Members/Luke A/Scripts/BubbleBreathing.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleBreathing : MonoBehaviour
+{
+	public Transform cameraTransform;
+
+	private Renderer _renderer;
+
+	private readonly Color _breatheColour = new Color(0f, 0.8f, 1f, 1f);
+	private readonly Color _holdColour = new Color(0.8f, 0f, 1f, 1f);
+
+	public List<SizeChanging> sizeChanges;
+
+	[Serializable]
+	public struct SizeChanging
+	{
+		public float holdDelay;
+		public float changeDelay;
+		public float newScale;
+	}
+	
+	void OnEnable()
+	{
+		_renderer = GetComponentInChildren<Renderer>();
+		if (sizeChanges.Count > 0) StartCoroutine(ChangeSizes());
+	}
+
+	private void Update()
+	{
+		transform.rotation = Quaternion.LookRotation(cameraTransform.position-transform.position);
+	}
+
+	IEnumerator ChangeSizes()
+	{
+		for (int i = 0; i < sizeChanges.Count; i++)
+		{
+			StartCoroutine(ChangeColour(sizeChanges[i].holdDelay));
+			yield return new WaitForSeconds(sizeChanges[i].holdDelay);
+			float oldScale = transform.localScale.x;
+			float newScale = sizeChanges[i].newScale;
+			float progress = 0;
+			float totalDelay = sizeChanges[i].changeDelay;
+			while (progress/totalDelay < 1)
+			{
+				yield return new WaitForFixedUpdate();
+				progress += Time.fixedDeltaTime;
+				transform.localScale = Mathf.Lerp(oldScale, newScale, progress/totalDelay) * Vector3.one;
+			}
+		}
+	}
+
+	IEnumerator ChangeColour(float delay)
+	{
+		float progress = 0;
+		while (progress/0.5f < 1)
+		{
+			yield return new WaitForFixedUpdate();
+			progress += Time.fixedDeltaTime;
+			_renderer.material.SetColor("_EmissionColor", Color.Lerp(_breatheColour, _holdColour, progress/0.5f));
+		}
+		yield return new WaitForSeconds(delay-1f);
+		progress = 0;
+		while (progress/0.5f < 1)
+		{
+			yield return new WaitForFixedUpdate();
+			progress += Time.fixedDeltaTime;
+			_renderer.material.SetColor("_EmissionColor", Color.Lerp(_holdColour, _breatheColour, progress/0.5f));
+		}
+	}
+}
---
+++ Assets/Team Members/Luke A/Scripts/MantaRayGlow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MantaRayGlow : MonoBehaviour
+{
+	public float lengthOfGlow;
+	
+	private Renderer _renderer;
+	
+	private readonly Color _noGlowColour = Color.black;
+	private readonly Color _fullGlowColour = new Color(0.9f, 0.9f, 0.9f, 1f);
+	
+	private void OnEnable()
+	{
+		_renderer = GetComponent<Renderer>();
+		StartCoroutine(ChangeColour(lengthOfGlow));
+	}
+
+	IEnumerator ChangeColour(float delay)
+	{
+		float progress = 0;
+		while (progress/delay < 0.5f)
+		{
+			yield return new WaitForFixedUpdate();
+			progress += Time.fixedDeltaTime;
+			_renderer.material.SetColor("_EmissionColor", Color.Lerp(_noGlowColour, _fullGlowColour, progress*2/delay));
+		}
+		progress = 0;
+		while (progress/delay < 0.5f)
+		{
+			yield return new WaitForFixedUpdate();
+			progress += Time.fixedDeltaTime;
+			_renderer.material.SetColor("_EmissionColor", Color.Lerp(_fullGlowColour, _noGlowColour, progress*2/delay));
+		}
+
+		gameObject.SetActive(false);
+	}
+}
