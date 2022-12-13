629bf99ac9e2a208d80594e8e707a6895bfda312
10213149 <1021349@student.sae.edu.au>
2022-12-13 14:27:44 +1100

    Luke M Audio update
    Another bunch of crashes later...
    bubble sounds and general SFX update to timeline

---
+++ Assets/Team Members/Luke M/Scripts/PitchRandomiser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitchRandomiser : MonoBehaviour
+{
+    public float time = 7f; //seconds
+    public AudioSource SFXSounds;
+
+
+    private void Start()
+    {
+        CallTimer();
+        /*GameManager gm = GameManager.Singleton;
+	    gm.DoThing += CallTimer;*/
+    }
+
+    void CallTimer()
+    {
+        StartCoroutine(Timer(time));
+    }
+
+
+    private IEnumerator Timer(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Debug.Log("Bloop!");
+        float pitch = Random.Range(0.85f, 1.25f);
+        this.SFXSounds.pitch = pitch;
+        time = 7;
+        CallTimer();
+    }
+}
